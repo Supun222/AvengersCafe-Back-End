85d238391f8d7403dc55a418e4f4faa977209369
Supun222 <74459467+Supun222@users.noreply.github.com>
2021-11-06 21:36:51 +0530

    First Commit

---
+++ src/test/java/com/example/AvengersBack/AvengersBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AvengersBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvengersBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
